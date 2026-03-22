ee4f337b3c134e14cbbdf590d8555d614ea76170
Datminh123 <minhdat2727@gmail.com>
2026-03-22 16:36:42 +0700

    update

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ dotenv.config();
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
-  cors: { origin: "http://localhost:5173",
+  cors: { origin: ["http://localhost:5173","https://finalproject2-fe.onrender.com"],
     methods: ["GET", "POST"],
     credentials: true
    }
