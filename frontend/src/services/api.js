1296b196ecaddd460c88b6b3fec735ee229c99f0
Datminh123 <minhdat2727@gmail.com>
2026-03-22 15:36:39 +0700

    new3

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,8 +1,12 @@
 
 import axios from "axios";
-
+const BASE_URL = import.meta.env.VITE_API_BASE_URL;
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: BASE_URL,
+  timeout: 10000,
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
 
 // ============ JOB API ============
